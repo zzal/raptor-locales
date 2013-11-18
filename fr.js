33b0fb291764642afeb75138c96338f59bb44f21
David Neilsen <david@panmedia.co.nz>
2013-11-18 13:50:33 +1300

    Init

---
+++ fr.js
@@ -0,0 +1,254 @@
+/**
+ * @fileOverview French strings file.
+ * @license http://www.raptor-editor.com/license
+ *
+ * @author SebCorbin, seb.corbin@gmail.com, https://github.com/SebCorbin/
+ * @author In4matik, https://github.com/In4matik/
+ */
+registerLocale('fr', 'Français', {
+    alignCenterTitle: 'Aligner au centre',
+    alignJustifyTitle: 'Justifier',
+    alignLeftTitle: 'Aligner à gauche',
+    alignRightTitle: 'Aligner à droite',
+
+    cancelDialogCancelButton: 'Fermer',
+    cancelDialogContent: 'Êtes-vous sûr(e) de vouloir arrêter la modification ? Toutes les modifications seront perdues !',
+    cancelDialogOKButton: 'OK',
+    cancelDialogTitle: 'Annuler l\'édition',
+    cancelTitle: 'Annuler l\'édition',
+
+    classMenuTitle: 'Sélecteur de style',
+    clearFormattingTitle: 'Effacer la mise en forme',
+    clickButtonToEditText: 'Cliquer pour commencer la modification',
+    clickButtonToEditTitle: null,
+
+    colorMenuBasicAutomatic: 'Automatique',
+    colorMenuBasicBlack: 'Noir',
+    colorMenuBasicBlue: 'Bleu',
+    colorMenuBasicGreen: 'Vert',
+    colorMenuBasicGrey: 'Gris',
+    colorMenuBasicOrange: 'Orange',
+    colorMenuBasicPurple: 'Violet',
+    colorMenuBasicRed: 'Rouge',
+    colorMenuBasicTitle: 'Changer le couleur du texte',
+    colorMenuBasicWhite: 'Blanc',
+
+    dockToElementTitle: 'Fixer l\'éditeur à l\'élément',
+    dockToScreenTitle: 'Fixer l\'éditeur à l\'écran',
+
+    embedTitle: 'Object intégré',
+    embedDialogTitle: 'Object intégré',
+    embedDialogTabCode: 'Code intégré',
+    embedDialogTabCodeContent: 'Collez votre code dans la zone de texte suivante :',
+    embedDialogTabPreview: 'Aperçu',
+    embedDialogTabPreviewContent: 'Un aperçu de votre code est disponible ci-dessous :',
+    embedDialogOKButton: 'Intégrer l\'objet',
+    embedDialogCancelButton: 'Annuler',
+
+    errorUINoName: 'UI "{{ui}}" est invalide (doit posséder une propriété "name")',
+    errorUINotObject: 'UI "{{ui}}" est invalide (doit être un objet)',
+    errorUIOverride: 'UI "{{name}}" a déjà été définie, et sera écrasée',
+
+    floatLeftTitle: 'Image à gauche du texte',
+    floatNoneTitle: 'Retirer l\'aligement de l\'image',
+    floatRightTitle: 'Image à droite du texte',
+
+    guidesTitle: 'Afficher les guides',
+
+    historyRedoTitle: 'Rétablir',
+    historyUndoTitle: 'Annuler',
+
+    hrCreateTitle: 'Insérer une ligne horizontale',
+
+    imageResizeButtonText: 'Redimmensionner l\'image',
+    imageResizeButtonDialogWidth: 'Largeur de l\'image',
+    imageResizeButtonDialogHeight: 'Hauteur de l\'image',
+    imageResizeButtonDialogWidthPlaceHolder: 'Largeur',
+    imageResizeButtonDialogHeightPlaceHolder: 'Hauteur',
+    imageResizeButtonDialogTitle: 'Redimmensionner l\'image',
+    imageResizeButtonDialogOKButton: 'Redimmensionner',
+    imageResizeButtonDialogCancelButton: 'Annuler',
+
+    insertFileTitle: 'Insérer un fichier',
+    insertFileDialogOKButton: 'Insérer le fichier',
+    insertFileDialogCancelButton: 'Anuuler',
+    insertFileURLLabel: 'URL du fichier',
+    insertFileNameLabel: 'Nom du fichier',
+    insertFileURLPlaceHolder: 'URL du fichier...',
+    insertFileNamePlaceHolder: 'Nom du fichier...',
+
+    languageMenuTitle: 'Changer de langue',
+    languageMenuEN: 'English',
+    languageMenuDE: 'Deutsch',
+    languageMenuES: 'Español',
+    languageMenuFR: 'Français',
+    languageMenuNL: 'Nederlandse',
+    languageMenuRU: 'Pусский',
+    languageMenuSV: 'Svensk',
+    languageMenuZHCN: '简体中文版',
+
+    listOrderedTitle: 'Liste ordonnée',
+    listUnorderedTitle: 'Liste non ordonnée',
+
+    linkCreateTitle: 'Insérer un lien',
+    linkRemoveTitle: 'Supprimer le lien',
+
+    linkCreateDialogTitle: 'Insérer un lien',
+    linkCreateDialogOKButton: 'Insérer un lien',
+    linkCreateDialogCancelButton: 'Annuler',
+    linkCreateDialogMenuHeader: 'Choisissez un type de lien :',
+
+    linkTypeEmailLabel: 'Adresse e-mail',
+    linkTypeEmailHeader: 'Lier une adresse e-mail',
+    linkTypeEmailToLabel: 'E-mail:',
+    linkTypeEmailToPlaceHolder: 'Saisir l\'adresse e-mail',
+    linkTypeEmailSubjectLabel: 'Sujet (facultatif):',
+    linkTypeEmailSubjectPlaceHolder: 'Saisir le sujet',
+
+    linkTypeExternalLabel: 'Page sur une autre site',
+    linkTypeExternalHeader: 'Lien vers une page sur un autre site',
+    linkTypeExternalLocationLabel: 'Emplacement',
+    linkTypeExternalLocationPlaceHolder: 'Entrer une URL',
+    linkTypeExternalNewWindowHeader: 'Nouvelle fenêtre',
+    linkTypeExternalNewWindowLabel: 'Cochez cette case pour ouvrir le lien dans une nouvelle fenêtre de navigateur.',
+    linkTypeExternalInfo:
+        '<h2>Pas sûr(e) de savoir quoi mettre dans le champ ci-dessus ?</h2>' +
+        '<ol>' +
+        '    <li>Trouvez la page web que vous voulez lier.</li>' +
+        '    <li>Copiez l\'adresse de la barre d\'adresse de votre navigateur et collez-la dans le champs ci-dessous.</li>' +
+        '</ol>',
+
+    linkTypeDocumentLabel: 'Document ou autre fichier',
+    linkTypeDocumentHeader: 'Lien vers un document ou un autre fichier',
+    linkTypeDocumentLocationLabel: 'Emplacement :',
+    linkTypeDocumentLocationPlaceHolder: 'Entrer une URL',
+    linkTypeDocumentNewWindowHeader: 'Nouvelle fenêtre',
+    linkTypeDocumentNewWindowLabel: 'Cochez cette case pour ouvrir le lien dans une nouvelle fenêtre de navigateur.',
+    linkTypeDocumentInfo:
+        '<h2>Pas sûr(e) de savoir quoi mettre dans le champ ci-dessus ?</h2>' +
+        '<ol>' +
+        '    <li>Assurez-vous que le fichier a bien été envoyé sur votr site</li>' +
+        '    <li>Ouvrez le fichier envoyé dans votre navigateur.</li>' +
+        '    <li>Copiez l\'adresse de la barre d\'adresse de votre navigateur et collez-la dans le champs ci-dessous.</li>' +
+        '</ol>',
+
+    linkTypeInternalLabel: 'Page de ce site',
+    linkTypeInternalHeader: 'Lien vers une page de ce site',
+    linkTypeInternalLocationLabel: '',
+    linkTypeInternalLocationPlaceHolder: 'Entrer une URI',
+    linkTypeInternalNewWindowHeader: 'Nouvelle fenêtre',
+    linkTypeInternalNewWindowLabel: 'Cochez cette case pour ouvrir le lien dans une nouvelle fenêtre de navigateur.',
+    linkTypeInternalInfo:
+        '<h2>Pas sûr(e) de savoir quoi mettre dans le champ ci-dessus ?</h2>' +
+        '<ol>' +
+        '    <li>Trouvez la page web que vous voulez lier.</li>' +
+        '    <li>Copiez l\'adresse de la barre d\'adresse de votre navigateur, sans "{{domain}}" et collez-la dans le champs ci-dessous..</li>' +
+        '</ol>',
+
+    logoTitle: 'En savoir plus sur l\'éditeur WYSIWYG Raptor',
+
+    navigateAway: '\nil y a des modifications non enregistrées sur ce site. Si vous quittez cette page, elles seront définitivements perdues',
+
+    pasteDialogTitle: 'Coller',
+    pasteDialogOKButton: 'Insérer',
+    pasteDialogCancelButton: 'Annuler',
+    pasteDialogPlain: 'Texte brute',
+    pasteDialogFormattedCleaned: 'Mis en forme &amp; nettoyé',
+    pasteDialogFormattedUnclean: 'Mis en forme sans nettoyage',
+    pasteDialogSource: 'Code source',
+
+    placeholderPluginDefaultContent: '&nbsp;',
+
+    saveTitle: 'Enregistrer le contenu',
+    saveJsonFail: 'Impossible d\'enregistrer {{failed}} bloc(s) de contenu',
+    saveJsonSaved: 'Enregistrement effectué de {{saved}} bloc(s) de contenu.',
+    saveRestFail: 'Impossible d\'enregistrer {{failed}} bloc(s) de contenu.',
+    saveRestPartial: 'Enregistrement de {{saved}} sur {{failed}} bloc(s) de contenu.',
+    saveRestSaved: 'Enregistrement effectué {{saved}} bloc(s) de contenu.',
+
+    snippetMenuTitle: 'Fragments',
+
+    specialCharactersDialogOKButton: 'OK',
+    specialCharactersDialogTitle: 'Insérer un caractère spécial',
+    specialCharactersHelp: 'Cliquez sur un caractère spécial pour l\'ajouter. Cliquez sur "OK" lorsque vous avez fini pour fermer la fenêtre.',
+    specialCharactersTitle: 'Insérer un caractère spécial',
+
+    statisticsButtonCharacterOverLimit: '{{charactersRemaining}} caractères au-dessus de la limite',
+    statisticsButtonCharacterRemaining: '{{charactersRemaining}} caractères restants',
+    statisticsButtonCharacters: '{{characters}} caractères',
+    statisticsDialogCharactersOverLimit: '{{characters}} caractères, {{charactersRemaining}} au-dessus de la limite',
+    statisticsDialogCharactersRemaining: '{{characters}} caractères, {{charactersRemaining}} restants',
+    statisticsDialogNotTruncated: 'Le contenu ne sera pas tronqué',
+    statisticsDialogOKButton: 'OK',
+    statisticsDialogSentence: '{{sentences}} phrase',
+    statisticsDialogSentences: '{{sentences}} phrases',
+    statisticsDialogTitle: 'Statistiques de contenu',
+    statisticsDialogTruncated: 'Le contenu contient plus de {{limit}} caractères et peut être tronqué',
+    statisticsDialogWord: '{{words}} mot',
+    statisticsDialogWords: '{{words}} mots',
+    statisticsTitle: 'Cliquez pour voir les statistiques',
+
+    tableCreateTitle: 'Créer un tableau',
+    tableDeleteColumnTitle: 'Supprimer une colonne du tableau',
+    tableDeleteRowTitle: 'Supprimer une ligne du tableau',
+    tableInsertColumnTitle: 'Insérer une colonne',
+    tableInsertRowTitle: 'Insérer une ligne',
+    tableMergeCellsTitle: 'Fusionner des cellules',
+    tableSplitCellsTitle: 'Tronquer des cellules',
+
+    tagMenuTagH1: 'Titre&nbsp;1',
+    tagMenuTagH2: 'Titre&nbsp;2',
+    tagMenuTagH3: 'Titre&nbsp;3',
+    tagMenuTagH4: 'Titre&nbsp;4',
+    tagMenuTagNA: 'N/A',
+    tagMenuTagP: 'Paragraphe',
+    tagMenuTagDiv: 'Div',
+    tagMenuTagPre: 'Pré-formaté',
+    tagMenuTagAddress: 'Address',
+    tagMenuTitle: 'Modifier le style de l\'élément',
+
+    tagTreeElementLink: 'Sélectionner l\'élément {{element}}',
+    tagTreeElementTitle: 'Cliquer pour sélectionner le contenu de l\'élément "{{element}}"',
+    tagTreeRoot: 'racine',
+    tagTreeRootLink: 'Sélectionner tout le contenu modifiable',
+    tagTreeRootTitle: 'Cliquer pour sélectionner tout le contenu modifiable',
+
+    textBlockQuoteTitle: 'Citation',
+    textBoldTitle: 'Gras',
+    textItalicTitle: 'Italique',
+    textStrikeTitle: 'Barré',
+    textSubTitle: 'Indice',
+    textSuperTitle: 'Exposant',
+    textUnderlineTitle: 'Souligné',
+    textSizeDecreaseTitle: 'Diminuer la taille du texte',
+    textSizeIncreaseTitle: 'Augmenter la taille du texte',
+
+    unsavedEditWarningText: 'Il y a des modifications non enregistrées sur cette page',
+
+    revisionsText: 'Révisions',
+    revisionsTextEmpty: 'Aucune révision',
+    revisionsTitle: null,
+    revisionsApplyButtonTitle: 'Revenir en arrière',
+    revisionsAJAXFailed: 'Impossible de récupérer les révisions',
+    revisionsApplyButtonDialogCancelButton: 'Annuler',
+    revisionsApplyButtonDialogOKButton: 'Revenir en arrière',
+    revisionsApplyButtonDialogTitle: 'Confirmer le retour en arrière',
+    revisionsApplyDialogContent: 'Cela remplacera le contenu actuel par celui de la révision sélectionnée.<br/>Le contenu actuel sera ajouté comme une révision, et sera visible dans la liste des révisions pour ce bloc.',
+    revisionsDialogCancelButton: 'Annuler',
+    revisionsDialogTitle: 'Voir le contenu des révisions',
+    revisionsButtonCurrent: 'Actuelle',
+    revisionsButtonViewDiffText: 'Différences',
+    revisionsButtonViewDiffTitle: null,
+    revisionsDiffButtonDialogCancelButton: 'Fermer',
+    revisionsDiffButtonDialogTitle: 'Voir les différences',
+    revisionsDiffButtonTitle: 'Voir les différences',
+    revisionsLoading: 'Chargement des révisions...',
+    revisionsNone: 'Pas de révision pour cet élément',
+    revisionsPreviewButtonTitle: 'Aperçu',
+
+    viewSourceDialogCancelButton: 'Fermer',
+    viewSourceDialogOKButton: 'Appliquer la source code',
+    viewSourceDialogTitle: 'Code source du contenu',
+    viewSourceTitle: 'Voir / Modifier la source code'
+
+});
---
+++ include.php
@@ -0,0 +1,8 @@
+<script type="text/javascript" src="<?= RAPTOR_LOCALES_URI; ?>en.js"></script>
+<script type="text/javascript" src="<?= RAPTOR_LOCALES_URI; ?>de.js"></script>
+<script type="text/javascript" src="<?= RAPTOR_LOCALES_URI; ?>es.js"></script>
+<script type="text/javascript" src="<?= RAPTOR_LOCALES_URI; ?>fr.js"></script>
+<script type="text/javascript" src="<?= RAPTOR_LOCALES_URI; ?>nl.js"></script>
+<script type="text/javascript" src="<?= RAPTOR_LOCALES_URI; ?>ru.js"></script>
+<script type="text/javascript" src="<?= RAPTOR_LOCALES_URI; ?>sv.js"></script>
+<script type="text/javascript" src="<?= RAPTOR_LOCALES_URI; ?>zh-CN.js"></script>
