c3217e46283fff890066f3d7cae5f2febc56633e
David Neilsen <david@panmedia.co.nz>
2013-12-10 14:38:07 +1300

    Update build files

--- build.js
+++ build.js
@@ -1,3 +1,68 @@
+
+builder.addLocale({
+    name: 'ar',
+    description: 'Arabic',
+    files: [
+        __dirname + '/ar.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'bg',
+    description: 'Bulgarian',
+    files: [
+        __dirname + '/bg.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'ca',
+    description: 'Catalan',
+    files: [
+        __dirname + '/ca.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'zh-CHS',
+    description: 'Chinese (Simplified)',
+    files: [
+        __dirname + '/zh-CHS.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'zh-CHT',
+    description: 'Chinese (Traditional)',
+    files: [
+        __dirname + '/zh-CHT.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'cs',
+    description: 'Czech',
+    files: [
+        __dirname + '/cs.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'da',
+    description: 'Danish',
+    files: [
+        __dirname + '/da.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'nl',
+    description: 'Dutch',
+    files: [
+        __dirname + '/nl.js'
+    ]
+});
+
 builder.addLocale({
     name: 'en',
     description: 'English',
@@ -7,34 +72,178 @@ builder.addLocale({
 });
 
 builder.addLocale({
-    name: 'de',
-    description: 'Deutsch',
+    name: 'et',
+    description: 'Estonian',
     files: [
-        __dirname + '/de.js'
+        __dirname + '/et.js'
     ]
 });
 
 builder.addLocale({
-    name: 'es',
-    description: 'Español',
+    name: 'fa',
+    description: 'Persian (Farsi)',
     files: [
-        __dirname + '/es.js'
+        __dirname + '/fa.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'fi',
+    description: 'Finnish',
+    files: [
+        __dirname + '/fi.js'
     ]
 });
 
 builder.addLocale({
     name: 'fr',
-    description: 'Français',
+    description: 'French',
     files: [
         __dirname + '/fr.js'
     ]
 });
 
 builder.addLocale({
-    name: 'nl',
-    description: 'Nederlands',
+    name: 'de',
+    description: 'German',
     files: [
-        __dirname + '/nl.js'
+        __dirname + '/de.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'el',
+    description: 'Greek',
+    files: [
+        __dirname + '/el.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'ht',
+    description: 'Haitian Creole',
+    files: [
+        __dirname + '/ht.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'he',
+    description: 'Hebrew',
+    files: [
+        __dirname + '/he.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'hi',
+    description: 'Hindi',
+    files: [
+        __dirname + '/hi.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'hu',
+    description: 'Hungarian',
+    files: [
+        __dirname + '/hu.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'id',
+    description: 'Indonesian',
+    files: [
+        __dirname + '/id.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'it',
+    description: 'Italian',
+    files: [
+        __dirname + '/it.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'ja',
+    description: 'Japanese',
+    files: [
+        __dirname + '/ja.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'ko',
+    description: 'Korean',
+    files: [
+        __dirname + '/ko.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'lv',
+    description: 'Latvian',
+    files: [
+        __dirname + '/lv.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'lt',
+    description: 'Lithuanian',
+    files: [
+        __dirname + '/lt.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'ms',
+    description: 'Malay',
+    files: [
+        __dirname + '/ms.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'mww',
+    description: 'Hmong Daw',
+    files: [
+        __dirname + '/mww.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'no',
+    description: 'Norwegian',
+    files: [
+        __dirname + '/no.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'pl',
+    description: 'Polish',
+    files: [
+        __dirname + '/pl.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'pt',
+    description: 'Portuguese',
+    files: [
+        __dirname + '/pt.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'ro',
+    description: 'Romanian',
+    files: [
+        __dirname + '/ro.js'
     ]
 });
 
@@ -47,17 +256,73 @@ builder.addLocale({
 });
 
 builder.addLocale({
+    name: 'sk',
+    description: 'Slovak',
+    files: [
+        __dirname + '/sk.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'sl',
+    description: 'Slovenian',
+    files: [
+        __dirname + '/sl.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'es',
+    description: 'Spanish',
+    files: [
+        __dirname + '/es.js'
+    ]
+});
+
+builder.addLocale({
     name: 'sv',
-    description: 'Svenska',
+    description: 'Swedish',
     files: [
         __dirname + '/sv.js'
     ]
 });
 
 builder.addLocale({
-    name: 'zh-CN',
-    description: '简体中文',
+    name: 'th',
+    description: 'Thai',
     files: [
-        __dirname + '/zh-CN.js'
+        __dirname + '/th.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'tr',
+    description: 'Turkish',
+    files: [
+        __dirname + '/tr.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'uk',
+    description: 'Ukrainian',
+    files: [
+        __dirname + '/uk.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'ur',
+    description: 'Urdu',
+    files: [
+        __dirname + '/ur.js'
+    ]
+});
+
+builder.addLocale({
+    name: 'vi',
+    description: 'Vietnamese',
+    files: [
+        __dirname + '/vi.js'
     ]
 });
