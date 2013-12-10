651b5c1c1c9571a2fb77bcab1e6a1beff939bb75
David Neilsen <david@panmedia.co.nz>
2013-12-10 14:37:48 +1300

    Update zh-CHS.js

---
+++ zh-CHS.js
@@ -0,0 +1,27 @@
+/**
+ * @fileOverview Simplified Chinese strings file.
+ * @license http://www.raptor-editor.com/license
+ *
+ * @author Raptor, info@raptor-editor.com, http://www.raptor-editor.com/
+ */
+extendLocale('zh-CHS', '简体中文', {
+    alignCenterTitle: '中心对齐文本',
+    alignJustifyTitle: '对齐文字',
+    alignLeftTitle: '左对齐文本',
+    alignRightTitle: '右对齐文本',
+
+    cancelDialogOKButton: '确定',
+
+
+    guidesTitle: '纲要',
+
+    linkDialogCancelButton: '取消',
+
+    linkTypeEmailLabel: '电子邮件',
+    statisticsDialogOKButton: '确定',
+    tagTreeRoot: '本',
+    textBlockQuoteTitle: '大段引用',
+    textBoldTitle: '粗体',
+    textItalicTitle: '斜体字',
+    textUnderlineTitle: '下划线',
+});
